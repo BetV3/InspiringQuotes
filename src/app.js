d7d7f59b9d70a1cb6f1e58510e89046b798a4d5b
Bet <elvisramirez999@gmail.com>
2024-01-17 01:21:00 +0000

    removed the test quotes

--- src/app.js
+++ src/app.js
@@ -11,12 +11,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to my API!');
 });
 
-app.get('/v1/quotes', (req, res) => {
-    res.json([
-        { author: 'Author 1', quote: 'Quote 1', tags: 'tag1' },
-        { author: 'Author 2', quote: 'Quote 2', tags: 'tag2' },
-    ]);
-});
 app.use('/api/', quotesRoutes);
 
 
