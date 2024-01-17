6814b1f13c58e6319f838e014979b55541f06054
Bet <elvisramirez999@gmail.com>
2024-01-17 00:37:36 +0000

    Added quote search and fixed routes. closes #4

--- src/controllers/quotesController.js
+++ src/controllers/quotesController.js
@@ -34,7 +34,20 @@ const getQuoteById = async (req, res) => {
 const getQuotesByLimit = async (req, res) => {
     // handles the /:limit route
     try {
+        console.log("Inside the getQuotesByLimit function")
         const quotes = await Quote.find({}, {_id: 0, __v: 0}).limit(parseInt(req.params.limit));
+        if (!quotes) {
+            return res.status(404).send('No quote found');
+        }
+        res.json(quotes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+const getQuotesByString = async (req, res) => {
+    try {
+        console.log(req.query.search);
+        const quotes = await Quote.find({$text: {$search: req.query.search}}, {_id: 0, __v: 0});
         if (!quotes) {
             return res.status(404).send('No quote found');
         }
@@ -48,4 +61,5 @@ module.exports = {
     getRandomQuote,
     getQuoteById,
     getQuotesByLimit,
+    getQuotesByString,
 };
--- src/routes/quoteRoutes.js
+++ src/routes/quoteRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const quotesController = require('../controllers/quotesController.js');
 
-
+// quotes listing will match first route and go to the callback function
 router.get('/random', quotesController.getRandomQuote);
 router.get('/id/:id', quotesController.getQuoteById);
+router.get('/search', quotesController.getQuotesByString);
 router.get('/:limit', quotesController.getQuotesByLimit);
+
 
 
 
